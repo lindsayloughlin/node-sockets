d9e40a49b20ebc51e1566f069b373bb9ceba4762
Lindsay Loughlin <lloughlin@odecee.com.au>
2017-07-13 17:19:42 +1000

    Include socket demo.

--- public/js/index.js
+++ public/js/index.js
@@ -9,16 +9,16 @@ var socket = io();
 socket.on('connect', function () {
     console.log('Connected to server');
 
-    socket.emit('createemail', {
-        to: 'some1@test.com',
-        text: 'hey you'
-    });
-
-    socket.emit('messagerecieved', {
-
-        from: 'someone',
-        text: 'some message'
-    });
+    // socket.emit('createemail', {
+    //     to: 'some1@test.com',
+    //     text: 'hey you'
+    // });
+    //
+    // socket.emit('messagerecieved', {
+    //
+    //     from: 'someone',
+    //     text: 'some message'
+    // });
 
 });
 
@@ -26,6 +26,9 @@ socket.on('disconnect', function () {
     console.log('disconnected from server');
 });
 
+socket.on('newMessage', function(message){
+   console.log(`Received message ${message}`);
+});
 
 
 socket.on('newEmail', function (message) {
--- server/server.js
+++ server/server.js
@@ -10,7 +10,7 @@ const socketIO = require('socket.io');
 const publicPath = path.join(__dirname, '../public');
 
 console.log(__dirname + '../public');
-console.log(publicPath );
+console.log(publicPath);
 
 var app = express();
 var server = http.createServer(app);
@@ -18,7 +18,7 @@ var io = socketIO(server);
 
 app.use(express.static(publicPath));
 
-io.on('connection', (socket)=>{
+io.on('connection', (socket) => {
     console.log('New user connected');
 
     // socket.emit('newEmail', (newEmail)=>{
@@ -26,18 +26,21 @@ io.on('connection', (socket)=>{
     // });
 
 
-    socket.emit('newMessage', {
-       from: 'John',
-       text: 'See you the',
-       createdAt: Date.now()
+    socket.on('createMessage', (message) => {
+        console.log(`recieved created message ${message}`);
+        io.emit('newMessage', {
+            from: message.from,
+            text: message.text,
+            created: new Date().getTime()
+        });
     });
 
-    socket.emit('createMessage', {
-        to: 'person',
-        text: 'message'
-    });
+    // socket.emit('createMessage', {
+    //     to: 'person',
+    //     text: 'message'
+    // });
 
-    socket.on('messagerecieved', (message)=>{
+    socket.on('messagerecieved', (message) => {
         console.log('messagerecieved ', message);
     });
 
@@ -45,13 +48,13 @@ io.on('connection', (socket)=>{
     //    console.log('create email' ,email);
     // });
 
-    socket.on('disconnect', ()=>{
+    socket.on('disconnect', () => {
         console.log('User was disconnected from server');
     });
 });
 
 
 let PORT_NUMBER = process.env.PORT || 3000;
-server.listen(PORT_NUMBER, ()=>{
+server.listen(PORT_NUMBER, () => {
     console.log(`hello world on port ${PORT_NUMBER}`);
 });
