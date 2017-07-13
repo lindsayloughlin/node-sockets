9ce4db68c5e19e2d45f927a4cc410a92ae5da9f1
Lindsay Loughlin <lloughlin@odecee.com.au>
2017-07-13 16:37:17 +1000

    Include socket demo.

--- server/server.js
+++ server/server.js
@@ -4,6 +4,8 @@
 
 const path = require('path');
 const express = require('express');
+const http = require('http');
+const socketIO = require('socket.io');
 
 const publicPath = path.join(__dirname, '../public');
 
@@ -11,11 +13,21 @@ console.log(__dirname + '../public');
 console.log(publicPath );
 
 var app = express();
+var server = http.createServer(app);
+var io = socketIO(server);
 
 app.use(express.static(publicPath));
 
-let PORT_NUMBER = process.env.PORT || 3000;
-app.listen(PORT_NUMBER, ()=>{
+io.on('connection', (socket)=>{
+    console.log('New user connected');
 
+    socket.on('disconnect', ()=>{
+        console.log('User was disconnected from server');
+    });
+});
+
+
+let PORT_NUMBER = process.env.PORT || 3000;
+server.listen(PORT_NUMBER, ()=>{
     console.log(`hello world on port ${PORT_NUMBER}`);
 });
