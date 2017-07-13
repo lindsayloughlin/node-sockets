ba32871531d093284f033dceba794d19945d5dba
Lindsay Loughlin <lloughlin@odecee.com.au>
2017-07-13 16:10:26 +1000

    add in server files

---
+++ server/server.js
@@ -0,0 +1,21 @@
+/**
+ * Created by lloughlin on 13/7/17.
+ */
+
+const path = require('path');
+const express = require('express');
+
+const publicPath = path.join(__dirname, '../public');
+
+console.log(__dirname + '../public');
+console.log(publicPath );
+
+var app = express();
+
+app.use(express.static(publicPath));
+
+let PORT_NUMBER = 3000;
+app.listen(PORT_NUMBER, ()=>{
+
+    console.log(`hello world on port ${PORT_NUMBER}`);
+});
